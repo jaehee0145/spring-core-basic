a3e5d3c993688bd3e2c5612bf7f04c1e5ae6225a
jaehee <jaeheepark0145@gmail.com>
2023-08-13 17:29:32 +0900

    refactor: 중복 제거

--- src/main/java/hello/springcorebasic/AppConfig.java
+++ src/main/java/hello/springcorebasic/AppConfig.java
@@ -10,10 +10,18 @@ import hello.springcorebasic.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private static RateDiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
