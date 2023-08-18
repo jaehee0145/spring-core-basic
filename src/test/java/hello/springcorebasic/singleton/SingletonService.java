d39ca154b7b54edbf37484fe525bb75e2f30de81
jaehee <jaeheepark0145@gmail.com>
2023-08-18 21:16:02 +0900

    docs: Singleton Pattern

---
+++ src/test/java/hello/springcorebasic/singleton/SingletonService.java
@@ -0,0 +1,12 @@
+package hello.springcorebasic.singleton;
+
+public class SingletonService {
+
+    private static final SingletonService instance = new SingletonService();
+
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+
+}
