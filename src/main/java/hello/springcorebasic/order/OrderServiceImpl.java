527f7b713c076d8e1884b422ddd6a8cdfa5e33dc
jaehee <jaeheepark0145@gmail.com>
2023-08-24 19:59:50 +0900

    feat: component scan 적용

--- src/main/java/hello/springcorebasic/order/OrderServiceImpl.java
+++ src/main/java/hello/springcorebasic/order/OrderServiceImpl.java
@@ -3,13 +3,18 @@ package hello.springcorebasic.order;
 import hello.springcorebasic.discount.DiscountPolicy;
 import hello.springcorebasic.member.Member;
 import hello.springcorebasic.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
